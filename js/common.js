104cbb63390f9499cea5c488d7993d6098755628
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-04-24 12:25:27 +0100

    Added info on about page and photo

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = 'ec2-99-81-194-20.eu-west-1.compute.amazonaws.com';
+var token = '0157f092-5c74-46bc-a554-dac275eba799';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
